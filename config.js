c007cd3f1cfa288ac97af363728163e6a55fc046
Satt-not-human <98022690+Satt-not-human@users.noreply.github.com>
2022-01-19 17:56:43 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,59 @@
+global.DeveloperMode = 'false' //true Or false
+global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/GHnvi284XJTGc529j6M865']
+global.Owner = {
+  // use the phone number with the country code, for example Indonesia '62' America '1' etc
+  '62895336282144': { // put your number here
+    name: 'the.sad.boy01',
+    isDev: true, // if true this number will send if bot have bug and developer mode true
+    isReport: true, // if true this number will send if user use command /report or /join
+    isCreator: true // if true this number will send if user use command /owner or /creator
+  },
+  '6288217277973': { // put your number here
+    name: 'Diablo botz 1',
+    isDev: false, // if true this number will send if bot have bug and developer mode true
+    isReport: true, // if true this number will send if user use command /report or /join
+    isCreator: false // if true this number will send if user use command /owner or /creator
+  },
+  '6287878505740': {
+    name: 'diablo botz 2',
+    isReport: true, // if true this number will send if user use command /report or /join
+  },
+  '6283143393763': {} // ignore if you don't want anything, like name dll
+}
+global.mods = ['62895336282144'] // Moderator?
+global.prems = [] // Premium user has unlimited limit
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  nzcha: 'http://nzcha-apii.herokuapp.com',
+  bg: 'http://bochil.ddns.net',
+  fdci: 'https://api.fdci.se',
+  dzx: 'https://api.dhamzxploit.my.id',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  pencarikode: 'https://pencarikode.xyz'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'd90a9e986e18778b',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.xyz': 'apivinz',
+  'https://pencarikode.xyz': 'pais'
+}
+
+// Sticker WM
+global.packname = 'wa.me/62895336282144'
+global.author = 'diablo Botz Is The Best'
+
+global.multiplier = 39 // The higher, The harder levelup
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
